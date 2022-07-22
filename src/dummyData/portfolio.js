efe0b78a60332aed7620064d3f61333262dc4b05
Okoro John <okorojohndarlington@gmail.com>
2022-07-22 10:15:56 +0100

    Updated

--- src/dummyData/portfolio.js
+++ src/dummyData/portfolio.js
@@ -10,14 +10,14 @@ export const portfolios = [
 		id: 2,
 		name: "MERN SOCIAL APP",
 		url: "mernsocialapp2.herokuapp.com",
-		desc: "The motive of every business is to make profit or to have a breakeven at a worst case scenario. This is a MERN eCommerce app developed with the intention of helping business growth. It has a streamlined user interface and interaction.",
+		desc: "Devdomain is a platform that connects users mostly developers. It is a platform where one can share ideas and market ones skills and get connected to clients. Users have to create profile and follow one another for wider connection. Users can also create, like/unlike,update, and delete posts as well as comment,like/unlike,update,delete comments and replies.",
 		img: "/assets/featured.jpeg",
 	},
 	{
 		id: 3,
 		name: "MERN BLOG APP",
 		url: "mernblogapp1.herokuapp.com",
-		desc: "The motive of every business is to make profit or to have a breakeven at a worst case scenario. This is a MERN eCommerce app developed with the intention of helping business growth. It has a streamlined user interface and interaction.",
+		desc: "This is a blog app that dishes out latest news to the site visitors. It is full of contents grouped in categories viz:sports,entertainment,music,food,culture,lifestyle,family,religion,technology,business,career,politics,education,investment and lot more. Users can create,comment,edit and delete posts.",
 		img: "/assets/blogpage.png",
 	},
 ];
